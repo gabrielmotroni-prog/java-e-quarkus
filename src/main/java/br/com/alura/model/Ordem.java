916c14f96739add27b5566e0e9119db4ffe75cd4
C1325300 Gabriel Dos Santos Motroni <gabrielmotroni@ibm.com>
2024-06-04 14:05:12 -0300

    feat: create user ok

---
+++ src/main/java/br/com/alura/model/Ordem.java
@@ -0,0 +1,45 @@
+package br.com.alura.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Ordem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double preco;
+
+    private String tipo;
+
+    private LocalDate data;
+
+    private String status;
+
+    @Column(name = "user_id") //JPA e informar qual é o nome do atributo da tabela do banco de dados que o nosso objeto fará o binding.
+    private Long userId;
+
+    public void setPreco(Double preco){
+        this.preco = preco;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+
+    public  void setData(LocalDate data){
+        this.data = data;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+    public void setUserId(Long userId){
+        this.userId = userId;
+    }
+
+}
---
+++ src/main/java/br/com/alura/resource/UsuarioResource.java
@@ -0,0 +1,20 @@
+package br.com.alura.resource;
+
+import br.com.alura.model.Usuario;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/usuarios")
+public class UsuarioResource {
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void inserir(Usuario usuario){
+        //persisitindo na base
+        Usuario.persist(usuario);
+    }
+}
